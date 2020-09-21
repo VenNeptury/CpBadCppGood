25a06ef599a25e5ea372b39b1add7ddf519e3f94
Venny <sgtmattis@gmail.com>
2020-09-21 18:15:25 +0200

    fixed some minor stuff, might need some additional work.

---
+++ src/NekoChxn/ApiResponse.hpp
@@ -0,0 +1,32 @@
+#include <nlohmann/json.hpp>
+#include <string>
+
+class ApiResponse
+{
+public:
+    std::string url;
+    std::string errorMessage;
+    int statusCode;
+    bool ok = true;
+    ApiResponse(cpr::Response res)
+    {
+        statusCode = res.status_code;
+        errorMessage = res.error.message;
+
+        if (statusCode > 299 || statusCode < 200)
+            ok = false;
+        try
+        {
+            auto json = nlohmann::json::parse(res.text);
+            if (json.contains("url"))
+                url = json["url"].get<std::string>();
+            else if (ok)
+                ok = false;
+        }
+        catch (nlohmann::detail::parse_error error)
+        {
+            if (ok)
+                ok = false;
+        }
+    }
+};
--- src/NekoChxn/main.cpp
+++ src/NekoChxn/main.cpp
@@ -1,34 +1,11 @@
 #include <iostream>
-#include <fstream>
 #include <cpr/cpr.h>
-#include <nlohmann/json.hpp>
-
-class ApiResponse
-{
-public:
-    std::string url;
-    std::string errorMessage;
-    int statusCode;
-    bool ok = true;
-    ApiResponse(cpr::Response res)
-    {
-        statusCode = res.status_code;
-        errorMessage = res.error.message;
-
-        if (statusCode > 299 || statusCode < 200)
-            ok = false;
-
-        auto json = nlohmann::json::parse(res.text);
-        if (json.contains("url"))
-            url = json["url"].get<std::string>();
-        else
-            ok = false;
-    }
-};
+#include "ApiResponse.hpp"
 
 int main(int argc, char **argv)
 {
     auto resRaw = cpr::Get(cpr::Url{"https://api.neko-chxn.xyz/v1/blush/img"});
+
     auto res = *new ApiResponse(resRaw);
 
     std::cout << (res.ok ? "OK! " : "RIP! ") << res.statusCode << ": " << (res.ok ? res.url : res.errorMessage) << '\n';
