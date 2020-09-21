244e8893310e70f91396f6f217bc9ef593ebc648
Venny <sgtmattis@gmail.com>
2020-09-21 17:04:18 +0200

    serjoscha87 sucks

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ public:
 
 int main(int argc, char **argv)
 {
-    auto resRaw = cpr::Get(cpr::Url{"https://api.neko-chxn.xyz/v1/blush/img"}, cpr::VerifySsl(0));
+    auto resRaw = cpr::Get(cpr::Url{"https://api.neko-chxn.xyz/v1/blush/img"});
     auto res = *new ApiResponse(resRaw);
 
     std::cout << (res.ok ? "OK! " : "RIP! ") << res.statusCode << ": " << (res.ok ? res.url : res.errorMessage) << '\n';
