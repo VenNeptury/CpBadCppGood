5f5998ec8dda48b5f52f8bea9841a0a4fbba79e8
Venny <sgtmattis@gmail.com>
2020-09-23 02:58:18 +0200

    For great justice.

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -8,6 +8,8 @@
 const std::string formatHelp(char *processName);
 void die(std::string message, int exitCode = 0);
 void handleBase64(int argc, char **argv);
+void handleRepeat(int argc, char **argv);
+void handleAlternate(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
@@ -17,6 +19,12 @@ int main(int argc, char **argv)
     }
 
     char *arg = argv[1];
+    char *args[argc - 2];
+    for (int i = 2; i < argc; i++)
+    {
+        args[i - 2] = argv[i];
+    }
+    argc -= 2;
 
     switch (hash(arg))
     {
@@ -27,7 +35,11 @@ int main(int argc, char **argv)
         break;
     case "base64"_:
     case "b64"_:
-        handleBase64(argc, argv);
+        handleBase64(argc, args);
+    case "repeat"_:
+        handleRepeat(argc, args);
+    case "alternate"_:
+        handleAlternate(argc, args);
     default:
     {
         std::ostringstream oss;
@@ -58,23 +70,23 @@ const std::string formatHelp(char *processName)
 
 void handleBase64(int argc, char **argv)
 {
-    if (argc < 3)
+    if (argc < 1)
         die("Too few arguments!");
 
-    char *action = argv[2];
+    char *action = argv[0];
     bool skippedAction = false;
     std::string rest = "";
     if (std::strcmp(action, "d") != 0 && std::strcmp(action, "e") != 0)
     {
         skippedAction = true;
     }
-    else if (argc == 3)
+    else if (argc == 1)
         die("Too few arguments!");
 
-    if (argc > (skippedAction ? 3 : 4))
+    if (argc > (skippedAction ? 1 : 2))
     {
         std::ostringstream oss;
-        for (int i = skippedAction ? 2 : 3; i < argc; i++)
+        for (int i = skippedAction ? 0 : 1; i < argc; i++)
             oss << argv[i] << ' ';
         rest += oss.str();
     }
@@ -99,4 +111,59 @@ void handleBase64(int argc, char **argv)
         oss << "Invalid argument \"" << action << "\". Valid options: [e|d]";
         die(oss.str(), 1);
     }
+}
+
+void handleRepeat(int argc, char **argv)
+{
+    if (argc < 2)
+        die("Too few arguments!");
+
+    int times;
+    try
+    {
+        times = std::stoi(argv[0]);
+    }
+    catch (std::exception const &e)
+    {
+        die("You did not provide a valid number for how many times the string should be repeated.");
+    }
+
+    std::string sentence;
+    for (int i = 1; i < argc; i++)
+    {
+        sentence += argv[i];
+        if (i != argc - 1)
+            sentence += ' ';
+    }
+
+    std::string output;
+    output.reserve(sentence.size() * times);
+    while (times--)
+        output += sentence;
+    die(output);
+}
+
+void handleAlternate(int argc, char **argv)
+{
+    if (argc < 1)
+        die("Too few arguments!");
+
+    std::string output;
+    bool alternated = false;
+
+    for (int i = 0; i < argc; i++)
+    {
+        std::string word = argv[i];
+        std::string alternatedWord;
+        for (int j = 0; j < word.size(); j++)
+        {
+            alternatedWord += alternated ? toupper(word.at(j)) : tolower(word.at(j));
+            alternated = !alternated;
+        }
+        output += alternatedWord;
+        if (i != argc - 1)
+            output += ' ';
+    }
+
+    die(output);
 }
